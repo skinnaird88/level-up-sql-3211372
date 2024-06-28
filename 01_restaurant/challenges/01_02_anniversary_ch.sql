069185d7d18dde4e230d38b37a9880153d25ce90
Stephen Kinnaird <104144587+skinnaird88@users.noreply.github.com>
2024-06-28 09:59:48 +0000

    First commit

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -3,5 +3,7 @@
 
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
-
+CREATE TABLE CustomerResponses(
+customerID INTEGER PRIMARY
+)
 -- Hint: SQLite offers the INTEGER and REAL datatypes
